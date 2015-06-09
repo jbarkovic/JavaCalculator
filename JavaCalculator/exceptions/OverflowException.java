cb05ac6182846b33600facf6a494d05a3beeaf23
Joshua Barkovic <joshbarkovic@gmail.com>
2015-06-09 12:21:05 -0400

    Added new traditiona calculator Exceptions.
    To provide user interfaces with enough information to display proper
    error messages to the user

---
+++ JavaCalculator/exceptions/OverflowException.java
@@ -0,0 +1,9 @@
+package exceptions;
+
+public class OverflowException extends Exception {
+	private static final long serialVersionUID = -9167408816983158770L;
+
+	public OverflowException() {
+	}
+
+}
---
+++ JavaCalculator/exceptions/PrecisionException.java
@@ -0,0 +1,32 @@
+package exceptions;
+
+public class PrecisionException extends Exception {
+
+	private static final long serialVersionUID = -6297453595740871925L;
+
+	public PrecisionException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public PrecisionException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PrecisionException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PrecisionException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public PrecisionException(String message, Throwable cause,
+			boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
