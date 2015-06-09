f43a849a093b84977f6fbaadd8941b4304299404
Joshua Barkovic <joshbarkovic@gmail.com>
2015-06-09 17:55:45 -0400

    Changed the precision to 1E-10
    the previous value (1E-128) was taking wayyy too long

--- JavaCalculator/platform/Platform.java
+++ JavaCalculator/platform/Platform.java
@@ -2,6 +2,6 @@ package platform;
 
 /* Holds usefull information about the target platform*/
 public class Platform {
-	public static final double PRECISION = 1E-128d;
+	public static final double PRECISION = 1E-10;
 	private Platform() {}
 }
